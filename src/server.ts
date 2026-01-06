9ac83ed02d0b8adeb70c45e96c2d52b11c739607
rafliafriza12 <rafliafriza90@gmail.com>
2026-01-06 21:44:57 +0700

    fix: fixed config database

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,9 @@ import connectDB from "./config/database";
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to database
-connectDB();
-
-// For local development
-if (process.env.NODE_ENV !== "production") {
+connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
   });
-}
-
-// Export for Vercel serverless
-export default app;
+});
